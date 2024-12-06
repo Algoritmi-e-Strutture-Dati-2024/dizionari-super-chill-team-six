b71a7e2f7cb2476b298e803b1b0e6c418ed34c1d
Leandro Flavio La Ciura <l.laciura@studenti.uniba.it>
2024-12-06 12:47:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,47 @@
-#include <string>
-#include "Dizionario.cpp"
+#include <iostream>
+#include "DizionarioEsteso.cpp"
 
-int main() {
+int main() { 
+    DizionarioEsteso<int> dizionario;
 
-    Dizionario<std::string> dict;
+    dizionario.inserisci("Casa", 3);
+    dizionario.inserisci("Palazzo", 20);
+    dizionario.inserisci("Cane", 8);
 
-    dict.inserisci("apple", "fruit");
-    dict.inserisci("car", "vehicle");
-    dict.inserisci("book", "object");
+    std::cout << "Contenuto del dizionario dopo gli inserimenti:\n";
+    dizionario.stampa();
 
-    std::cout << "Value for 'apple': " << dict.recupera("apple") << "\n";
-    std::cout << "Value for 'car': " << dict.recupera("car") << "\n";
-    std::cout << "Value for 'pen': " << dict.recupera("pen") << "\n";
+    dizionario.inserisci("Csaa", 6);
+    dizionario.inserisci("naCe", 7);
+    
+    std::cout << "\nContenuto del dizionario dopo aggiunta delle voci con collisioni:\n";
+    dizionario.stampa();
 
-    dict.stampa();
+    std::cout << "\nValore associato a 'Casa': " << dizionario.recupera("Casa") << "\n";
+    std::cout << "Valore associato a 'naCe': " << dizionario.recupera("naCe") << "\n";
+
+    if (dizionario.appartiene("Palazzo")) {
+        std::cout << "'Palazzo' è presente nel dizionario.\n";
+    }
+    if (!dizionario.appartiene("Gatto")) {
+        std::cout << "'Gatto' non è presente nel dizionario.\n";
+    }
+
+    if (dizionario.cancella("Casa")) {
+        std::cout << "'Casa' è stato rimosso dal dizionario.\n";
+    } else {
+        std::cout << "'Casa' non è stato trovato nel dizionario.\n";
+    }
+
+    std::cout << "\nContenuto del dizionario dopo la rimozione di 'Casa':\n";
+    dizionario.stampa();
+
+    if (!dizionario.cancella("Gatto")) {
+        std::cout << "'Gatto' non esiste nel dizionario, impossibile rimuoverlo.\n";
+    }
+
+    std::cout << "\nSituazione finale del dizionario:\n";
+    dizionario.stampa();
 
     return 0;
 }
